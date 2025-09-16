56eb6c13f8837e270837cec6a917b7cbe551c646
Hélio Silva <hsilva@advanzia.com>
2025-09-16 17:39:23 +0200

    Paging

---
+++ BuildingBlocks/Application.Abstractions/Paging/PageRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Application.Abstractions.Paging;
+
+public sealed record PageRequest(int Page = 1, int PageSize = 20)
+{
+    public int Skip => (Page < 1 ? 0 : (Page - 1)) * (PageSize < 1 ? 20 : PageSize);
+    public int Take => PageSize < 1 ? 20 : PageSize;
+    public static PageRequest Default => new(1, 20);
+}
---
+++ BuildingBlocks/Application.Abstractions/Paging/PageResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Application.Abstractions.Paging;
+
+public sealed record PageResult<T>(IReadOnlyCollection<T> Items, int Page, int PageSize, long TotalCount)
+{
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasNext => Page < TotalPages;
+    public bool HasPrevious => Page > 1;
+}
